bfe3387e70c1a22c849071e786418e7831f0e108
urezan <pavel.semashko@gmail.com>
2015-04-10 20:32:08 +0300

    Pep8-imized

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ import png
 import sys
 from threading import Thread
 
+
 def clamp(x, l, h):
     return min(max(x, l), h)
+
 
 def norclamp(x, l, h):
     return (clamp(x, l, h) - l) / (h - l)
 
+
 def flt_to_byte(*args):
     return map(lambda x: int(255 * x),
                args)
+
 
 def heatmap_color(val, lo, hi):
     if val > hi or val < lo:
@@ -22,25 +26,30 @@ def heatmap_color(val, lo, hi):
     val = clamp(val, lo, hi)
     m = [0, 0, 0]
     for i in xrange(3):
-        m[i] = ((3 - i) * lo + (1 + i) * hi) / 4;
+        m[i] = ((3 - i) * lo + (1 + i) * hi) / 4
 
     b = 1.0 - norclamp(val, m[0], m[1])
     g = norclamp(val,   lo, m[0]) * (1.0 - norclamp(val, m[2],   hi))
     r = norclamp(val, m[1], m[2])
     col = flt_to_byte(b, g, r)
-    return col;
+    return col
+
 
 def fakelog(x):
-    d = x - 1;
-    return d - 0.5 * d * d 
+    d = x - 1
+    return d - 0.5 * d * d
+
 
 def log_color(val, lo, hi):
-    return heatmap_color(fakelog(1 + val / hi), fakelog(1 + lo / hi), fakelog(2))
+    return heatmap_color(fakelog(1 + val / hi),
+                         fakelog(1 + lo / hi),
+                         fakelog(2))
+
 
 def mandelbrot(z0):
     max_it = 255
-    it = 0;
-    z = z0;
+    it = 0
+    z = z0
     while (it < max_it and z.real * z.real + z.imag * z.imag < 4):
         z = z * z + z0
         it += 1
@@ -55,14 +64,13 @@ def calculate(a, y_low, y_high, step):
         if ((y - y_low) / step) % 10 == 0:
             print "%d%% done" % ((100 * (y - y_low)) // (y_high - y_low))
         for x in xrange(dim_x):
-            #z = complex(-2.5 + x * 3.5 / dim_x,
-            #             1.25 - y * 2.5 / dim_y)
-            z = complex(-1.5 + x * 0.5 / dim_x,
-                         0.50 - y * 0.5 / dim_y)
+            z = complex(-2.5 + x * 3.5 / dim_x,
+                        1.25 - y * 2.5 / dim_y)
             col = mandelbrot(z)
             a[y].append(col[0])
             a[y].append(col[1])
             a[y].append(col[2])
+
 
 def main():
     filename = sys.argv[1] if len(sys.argv) > 1 else "out.png"
@@ -75,8 +83,8 @@ def main():
     a = [[] for i in xrange(dim_y)]
     threads = []
     for batch in xrange(NUM_THREADS):
-        threads.append(Thread(target = calculate, args=(a, batch, dim_y, NUM_THREADS)))
-            #batch * batch_size, (batch + 1) * batch_size)))
+        threads.append(Thread(target=calculate,
+                              args=(a, batch, dim_y, NUM_THREADS)))
         threads[batch].start()
     for t in threads:
         t.join()
@@ -86,6 +94,7 @@ def main():
     w = png.Writer(wi, he)
     with open(filename, "wb") as f:
         w.write(f, a)
+
 
 if __name__ == "__main__":
     main()
